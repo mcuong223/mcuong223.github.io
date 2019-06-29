f0110bad5ce38434048d16ab905b12b417c83b1d
Cuong Pham <cuongpham@Cuongs-MacBook-Pro.local>
2019-06-29 21:21:26 +0700

    upgrade to nodejs/reactjs

---
+++ precache-manifest.9d43eee86b4a1c1325389c1c50b1d6f8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d485dadab43e772548d87439a6cd42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "379d9cc97dbb76d5f66c",
+    "url": "/static/js/2.464f01e6.chunk.js"
+  },
+  {
+    "revision": "07de7b33e19f3f721f1d",
+    "url": "/static/js/main.1457c17d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
